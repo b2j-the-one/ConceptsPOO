eef1b4a9fb5242690106b04110a615d308c0bc69
b2jtheone <b2jtheone@gmail.com>
2022-03-24 07:15:11 +0100

    Finish Date class

---
+++ ConceptsPOO/ConceptsPOO/MonthdException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class MonthdException : Exception
+    {
+        public MonthdException()
+        {
+        }
+
+        public MonthdException(string? message) : base(message)
+        {
+        }
+
+        public MonthdException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected MonthdException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ConceptsPOO;
+
+Console.WriteLine("Les concepts de la POO");
+Console.WriteLine("======================");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
